edda988aec7cb5abc3e3219910fc525583ce72ac
zaoqi <zaoqiff@gmail.com>
2017-03-12 16:41:57 +0800

    实现打印所有包

--- apt.sh
+++ apt.sh
@@ -1,27 +1,43 @@
 #!/bin/sh
+dvd="$1"
 pool="$1/pool"
-if [ ! -d "$pool" ] ;then
+if [ ! -d "$1" ] ;then
 	echo "使用方法：apt.sh <Debian DVD 目录绝对路径>" 1>&2
 	exit 1
 fi
 undeb() {
-	cd $(mktemp -d)
-	ar -xv "$1" 1>&2 || error
+	cd "$(mktemp -d)"
+	ar -xv "$*" 1>&2 || error
 	pwd
 	cd - >/dev/null
 }
 debinfo() {
-	cd $(mktemp -d)
-	tar -xvf "$1"/control.tar.* 1>&2 || error
+	cd "$(mktemp -d)"
+	tar -xvf "$*"/control.tar.* 1>&2 || error
 	cat control
 	cd - >/dev/null
 	rm -rfv $OLDPWD 1>&2 || error
 }
 debdep() {
-	debinfo $1 | grep Depends | sed 's/Depends: \(.*\)/\1/' | sed 's/([^)]*)//g' | sed 's/ *, */ /g'
+	debinfo "$*" | grep Depends | sed 's/Depends: \(.*\)/\1/' | sed 's/([^)]*)//g' | sed 's/ *, */ /g'
+}
+packages() {
+	for f in "$*/dists/jessie"/*/binary-armel/Packages.gz ;do
+		gzip -dc "$f"
+	done
+}
+untilNN() {
+	while read line ;do
+		if [ "$line" = "" ] ;then
+			return
+		else
+			echo "$line"
+		fi
+	done
 }
 error() {
 	echo "ERROR" 1>&2
 	exit 1
 }
-debdep $(undeb "$pool/main/a/apt"/apt_*_armel.deb)
+#debdep $(undeb "$pool/main/a/apt"/apt_*_armel.deb)
+packages "$dvd"
