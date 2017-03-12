880019e1a840f06c79643b7f5036311198224fa0
zaoqi <zaoqiff@gmail.com>
2017-03-12 16:53:07 +0800

    可以找到deb文件

--- apt.sh
+++ apt.sh
@@ -5,12 +5,14 @@ if [ ! -d "$pool" ] ;then
 	echo "使用方法：apt.sh <Debian DVD 目录绝对路径>" 1>&2
 	exit 1
 fi
+#参数：deb文件
 undeb() {
 	cd "$(mktemp -d)"
 	ar -xv "$*" 1>&2 || error
 	pwd
 	cd - >/dev/null
 }
+#参数：undeb得到的
 debinfo() {
 	cd "$(mktemp -d)"
 	tar -xvf "$*"/control.tar.* 1>&2 || error
@@ -18,14 +20,17 @@ debinfo() {
 	cd - >/dev/null
 	rm -rfv $OLDPWD 1>&2 || error
 }
+#参数：undeb得到的
 debdep() {
 	debinfo "$*" | grep Depends | sed 's/Depends: \(.*\)/\1/' | sed 's/([^)]*)//g' | sed 's/ *, */ /g'
 }
+#参数：$dvd
 packages() {
 	for f in "$*/dists/jessie"/*/binary-armel/Packages.gz ;do
-		gzip -dc "$f"
+		gzip -dc "$f" || error
 	done
 }
+#参数：$dvd 包名
 package() {
 	packages "$1" | grep -A 100 'Package: '"$2" | untilNN
 }
@@ -38,9 +43,14 @@ untilNN() {
 		fi
 	done
 }
+#参数：$dvd 包名
+getdeb() {
+	echo -n "$1/"
+	package "$1" "$2" | grep Filename | sed 's/Filename: \(.*\)/\1/g'
+}
 error() {
 	echo "ERROR" 1>&2
 	exit 1
 }
 #debdep $(undeb "$pool/main/a/apt"/apt_*_armel.deb)
-package "$dvd" python-wstools
+getdeb "$dvd" python-wstools
