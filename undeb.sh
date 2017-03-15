4cc4276b061aaad36a5432561dca547e5a922567
zaoqi <zaoqiff@gmail.com>
2017-03-15 20:40:59 +0800

    undeb.sh支持多文件

--- undeb.sh
+++ undeb.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-d="$(mktemp -d)"
-t="$(mktemp)"
-cp -f "$*" $t
-cd $d
-ar -xv $t
-cd -
-tar -xvf $d/data.tar.*
-rm -rfv $t $d
+#Simple APT
+#Copyright (C) 2017  zaoqi
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published
+#by the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+for file in "$@" ;do
+	d="$(mktemp -d)"
+	t="$(mktemp)"
+	cp -f "$file" $t
+	cd $d
+	ar -xv $t
+	cd -
+	tar -xvf $d/data.tar.*
+	rm -rfv $t $d
+done
